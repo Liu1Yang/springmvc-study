3f17b0a1fa227cd5f11428db52db5e0e000882af
LiuYang <3354994885@qq.com>
2026-06-14 21:33:06 +0800

    Spring,SpringMvc 分别对应bean加载

--- src/main/java/com/itheima/config/ServletContainersInitConfig.java
+++ src/main/java/com/itheima/config/ServletContainersInitConfig.java
@@ -2,23 +2,43 @@ package com.itheima.config;
 
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 import org.springframework.web.servlet.support.AbstractDispatcherServletInitializer;
 
-public class ServletContainersInitConfig extends AbstractDispatcherServletInitializer {
+public class ServletContainersInitConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+
     @Override
-    protected WebApplicationContext createServletApplicationContext() {
-        AnnotationConfigWebApplicationContext ctx=new AnnotationConfigWebApplicationContext();
-        ctx.register(SpringMvcConfig.class);
-        return ctx;
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{SpringConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringMvcConfig.class};
     }
 
     @Override
     protected String[] getServletMappings() {
         return new String[]{"/"};
     }
-
-    @Override
-    protected WebApplicationContext createRootApplicationContext() {
-        return null;
-    }
 }
+
+//public class ServletContainersInitConfig extends AbstractDispatcherServletInitializer {
+//    @Override
+//    protected WebApplicationContext createServletApplicationContext() {
+//        AnnotationConfigWebApplicationContext ctx=new AnnotationConfigWebApplicationContext();
+//        ctx.register(SpringMvcConfig.class);
+//        return ctx;
+//    }
+//
+//    @Override
+//    protected String[] getServletMappings() {
+//        return new String[]{"/"};
+//    }
+//
+//    @Override
+//    protected WebApplicationContext createRootApplicationContext() {
+//        return null;
+//    }
+//}
--- src/main/java/com/itheima/config/SpringMvcConfig.java
+++ src/main/java/com/itheima/config/SpringMvcConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 // @Configuration是Spring 框架 中的一个注解，用于标记一个类是配置类。
-@Configuration
+//@Configuration
 @ComponentScan("com.itheima.controller")
 public class SpringMvcConfig {
 }
