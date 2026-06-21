8b9085496a8b9f01882222f0c06259bab9d46607
LiuYang <3354994885@qq.com>
2026-06-21 23:48:09 +0800

    RESTful入门案例

---
+++ springmvc_06_rest/src/main/java/com/itheima/controller/UserController.java
@@ -0,0 +1,49 @@
+package com.itheima.controller;
+
+import com.itheima.domain.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class UserController {
+
+    @RequestMapping(value = "/users",method = RequestMethod.POST)
+    @ResponseBody
+    public  String save(){
+        System.out.println("user save...");
+        return "{'module':'user save'}";
+    }
+
+    // {id} 代表路径参数
+    @RequestMapping(value="/users/{id}",method = RequestMethod.DELETE)
+    @ResponseBody
+    public String delete(@PathVariable("id") Integer id){
+        System.out.println("user delete..."+id);
+        return "{'module':'user delete'}";
+    }
+
+    @RequestMapping(value = "/users",method = RequestMethod.PUT)
+    @ResponseBody
+    public String update(User user){
+        System.out.println("user update..."+user);
+        return "{'module':'user update'}";
+    }
+
+    @RequestMapping(value = "/users/{id}",method = RequestMethod.GET)
+    @ResponseBody
+    public String getById(@PathVariable("id") Integer id){
+        System.out.println("user getById..."+id);
+        return "{'module':'user getById'}";
+    }
+
+    @RequestMapping(value = "/users",method = RequestMethod.GET)
+    @ResponseBody
+    public String getAll(){
+        System.out.println("user getAll...");
+        return "{'module':'user getAll'}";
+    }
+
+}
