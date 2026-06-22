135d4154efeeb70a02ea662c2dde46c292bf3743
LiuYang <3354994885@qq.com>
2026-06-22 23:01:11 +0800

    简化部分注解

--- springmvc_06_rest/src/main/java/com/itheima/controller/BookController.java
+++ springmvc_06_rest/src/main/java/com/itheima/controller/BookController.java
@@ -4,39 +4,42 @@ import com.itheima.domain.Book;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+//@Controller
+//@ResponseBody // 代表这个类所有的方法都带@ReSponseBody
+@RestController // @Controller 和 @ResponseBody 合二为一
+@RequestMapping("/books") // 所有方法统一加个前缀/books
 public class BookController {
 
-    @RequestMapping(value = "/books",method = RequestMethod.POST)
-    @ResponseBody
+//    @RequestMapping(method = RequestMethod.POST)
+    @PostMapping  // 与上面的注解同一个意思
     public String save(@RequestBody Book book){
         System.out.println("book save..."+book);
         return "{'module':'book save}";
     }
 
-    @RequestMapping(value = "/books",method = RequestMethod.DELETE)
-    @ResponseBody
+//    @RequestMapping(value = "/{id}",method = RequestMethod.DELETE)
+    @DeleteMapping("/{id}")
     public String delete(@PathVariable Integer id){
         System.out.println("book delete..."+id);
         return "{'module':'book delete'}";
     }
 
-    @RequestMapping(value = "/books",method = RequestMethod.PUT)
-    @ResponseBody
+//    @RequestMapping(method = RequestMethod.PUT)
+    @PutMapping
     public String update(@RequestBody Book book){
         System.out.println("book update..."+book);
         return "{'module':'book update'}";
     }
 
-    @RequestMapping(value = "/books/{id}",method = RequestMethod.GET)
-    @ResponseBody
+//    @RequestMapping(value = "/{id}",method = RequestMethod.GET)
+    @GetMapping("/{id}")
     public String getById(@PathVariable("id") Integer id){
         System.out.println("book getById..."+id);
         return "{'module':'book getById'}";
     }
 
-    @RequestMapping(value = "/books/",method = RequestMethod.GET)
-    @ResponseBody
+//    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     public String getAll(){
         System.out.println("book getAll...");
         return "{'module':'book getById'}";
--- springmvc_06_rest/src/main/java/com/itheima/controller/UserController.java
+++ springmvc_06_rest/src/main/java/com/itheima/controller/UserController.java
@@ -4,40 +4,47 @@ import com.itheima.domain.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+//@Controller
+@RestController
+@RequestMapping("/users")
 public class UserController {
 
-    @RequestMapping(value = "/users",method = RequestMethod.POST)
-    @ResponseBody
+//    @RequestMapping(value = "/users",method = RequestMethod.POST)
+//    @ResponseBody
+    @PostMapping
     public  String save(){
         System.out.println("user save...");
         return "{'module':'user save'}";
     }
 
     // {id} 代表路径参数
-    @RequestMapping(value="/users/{id}",method = RequestMethod.DELETE)
-    @ResponseBody
+//    @RequestMapping(value="/users/{id}",method = RequestMethod.DELETE)
+//    @ResponseBody
+    @DeleteMapping("/{id}")
     public String delete(@PathVariable("id") Integer id){
         System.out.println("user delete..."+id);
         return "{'module':'user delete'}";
     }
 
-    @RequestMapping(value = "/users",method = RequestMethod.PUT)
-    @ResponseBody
+//    @RequestMapping(value = "/users",method = RequestMethod.PUT)
+    @PutMapping
+//    @ResponseBody
     public String update(@RequestBody User user){
         System.out.println("user update..."+user);
         return "{'module':'user update'}";
     }
 
-    @RequestMapping(value = "/users/{id}",method = RequestMethod.GET)
-    @ResponseBody
+//    @RequestMapping(value = "/users/{id}",method = RequestMethod.GET)
+//    @ResponseBody
+    @GetMapping("/{id}")
     public String getById(@PathVariable("id") Integer id){
         System.out.println("user getById..."+id);
         return "{'module':'user getById'}";
     }
 
-    @RequestMapping(value = "/users",method = RequestMethod.GET)
-    @ResponseBody
+//    @RequestMapping(value = "/users",method = RequestMethod.GET)
+//    @ResponseBody
+    @GetMapping
     public String getAll(){
         System.out.println("user getAll...");
         return "{'module':'user getAll'}";
