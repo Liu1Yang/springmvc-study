769e698bf9f8f1fcb248b87c49c999e764fb5ae8
LiuYang <3354994885@qq.com>
2026-06-27 00:25:01 +0800

    ssm整合（功能模块开发）

---
+++ springmvc_08_ssm/src/main/java/com/itheima/service/BookService.java
@@ -0,0 +1,40 @@
+package com.itheima.service;
+
+import com.itheima.domain.Book;
+
+import java.util.List;
+
+public interface BookService { // 提供业务层接口
+
+    /**
+     * 保存
+     * @param book
+     * @return
+     */
+    public boolean save(Book book);
+
+    /**
+     * 修改
+     * @param book
+     */
+    public boolean update(Book book);
+
+    /**
+     * 按id删除
+     * @param id
+     */
+    public boolean delete(Integer id);
+
+    /**
+     * 按id查询
+     * @param id
+     * @return
+     */
+    public Book getById(Integer id);
+
+    /**
+     * 查询全部
+     * @return
+     */
+    public List<Book> getAll();
+}
---
+++ springmvc_08_ssm/src/main/java/com/itheima/service/impl/BookServiceImpl.java
@@ -0,0 +1,44 @@
+package com.itheima.service.impl;
+
+import com.itheima.dao.BookDao;
+import com.itheima.domain.Book;
+import com.itheima.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookServiceImpl implements BookService {
+
+    @Autowired
+    private BookDao bookDao; // 如果ssm整合的时候，你要注入的东西当前在系统中不存在，IDEA会报错提示
+
+    @Override
+    public boolean save(Book book) {
+        bookDao.save(book);
+        return true;
+    }
+
+    @Override
+    public boolean update(Book book) {
+        bookDao.update(book);
+        return true;
+    }
+
+    @Override
+    public boolean delete(Integer id) {
+        bookDao.delete(id);
+        return true;
+    }
+
+    @Override
+    public Book getById(Integer id) {
+        return bookDao.getById(id);
+    }
+
+    @Override
+    public List<Book> getAll() {
+        return bookDao.getAll();
+    }
+}
