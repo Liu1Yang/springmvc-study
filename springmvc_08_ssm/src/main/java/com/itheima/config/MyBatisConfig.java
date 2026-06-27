98cf896409fe4d928ffabbabe40016a090dbf439
LiuYang <3354994885@qq.com>
2026-06-27 23:08:00 +0800

    补上@Bean注解

--- springmvc_08_ssm/src/main/java/com/itheima/config/MyBatisConfig.java
+++ springmvc_08_ssm/src/main/java/com/itheima/config/MyBatisConfig.java
@@ -2,11 +2,13 @@ package com.itheima.config;
 
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.mapper.MapperScannerConfigurer;
+import org.springframework.context.annotation.Bean;
 
 import javax.sql.DataSource;
 
 public class MyBatisConfig {
 
+    @Bean
     public SqlSessionFactoryBean sqlSessionFactoryBean(DataSource dataSource){
         SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean(); // 创建 MyBatis 的核心工厂，设置数据源和实体类包路径（用于自动别名）。
         factoryBean.setDataSource(dataSource);  // 注入数据源
@@ -14,6 +16,7 @@ public class MyBatisConfig {
         return factoryBean;
     }
 
+    @Bean
     public MapperScannerConfigurer mapperScannerConfigurer(){  // 扫描 dao 包下的所有 Mapper 接口
         // ，动态生成代理对象并注册到 Spring 容器，这样 Service 里就可以直接 @Autowired 那些 Mapper 接口了。
         MapperScannerConfigurer msc=new MapperScannerConfigurer();
