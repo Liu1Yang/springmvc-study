ea327a83824ba740f4d28472275cecd2f30f1e5b
LiuYang <3354994885@qq.com>
2026-06-29 21:29:54 +0800

    前后端协议联调

--- springmvc_08_ssm/src/main/java/com/itheima/config/SpringMvcConfig.java
+++ springmvc_08_ssm/src/main/java/com/itheima/config/SpringMvcConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@ComponentScan("com.itheima.controller")  // 只扫描 Controller 层。
+@ComponentScan({"com.itheima.controller","com.itheima.config"})  // 只扫描 Controller 层。
 @EnableWebMvc  //开启 SpringMVC 注解支持（相当于 <mvc:annotation-driven>），
 // 会自动注册 RequestMappingHandlerMapping 和 RequestMappingHandlerAdapter，并启用 JSON 转换、类型转换等。
 public class SpringMvcConfig {  // MVC 容器配置（表现层）
